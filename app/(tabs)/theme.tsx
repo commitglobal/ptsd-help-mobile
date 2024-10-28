175e4d31bc1d47a7b7e51dd4412b5d5171689409
luciatugui <lucia.tugui@wearetribus.com>
2024-10-28 13:20:08 +0200

    feat: app theming

---
+++ app/(tabs)/theme.tsx
@@ -0,0 +1,91 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Image, Platform } from "react-native";
+
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { XStack, YStack } from "@tamagui/stacks";
+import { SizableText } from "tamagui";
+
+const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Theme</ThemedText>
+      </ThemedView>
+
+      <Collapsible title="Blue">
+        <XStack gap="$4" borderColor="$blue11" flexWrap="wrap">
+          {colors.map((color) => (
+            <YStack
+              width="$4"
+              height="$4"
+              backgroundColor={`$blue${color}`}
+              borderRadius="$4"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <SizableText>{`${color}`}</SizableText>
+            </YStack>
+          ))}
+        </XStack>
+      </Collapsible>
+
+      <Collapsible title="Orange">
+        <XStack gap="$4" borderColor="$orange11" flexWrap="wrap">
+          {colors.map((color) => (
+            <YStack
+              width="$4"
+              height="$4"
+              backgroundColor={`$orange${color}`}
+              borderRadius="$4"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <SizableText>{`${color}`}</SizableText>
+            </YStack>
+          ))}
+        </XStack>
+      </Collapsible>
+
+      <Collapsible title="Gray">
+        <XStack gap="$4" borderColor="$gray11" flexWrap="wrap">
+          {colors.map((color) => (
+            <YStack
+              width="$4"
+              height="$4"
+              backgroundColor={`$gray${color}`}
+              borderRadius="$4"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <SizableText>{`${color}`}</SizableText>
+            </YStack>
+          ))}
+        </XStack>
+      </Collapsible>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
