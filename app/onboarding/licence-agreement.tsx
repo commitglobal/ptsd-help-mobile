463d9f9d184c2d37ae16297f30d404f4d2c5e5aa
luciatugui <lucia.tugui@wearetribus.com>
2024-11-08 11:15:38 +0200

    feat: licence-agreement commit global logo

--- app/onboarding/licence-agreement.tsx
+++ app/onboarding/licence-agreement.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "expo-router";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Circle, ScrollView, XStack } from "tamagui";
+import { Circle, Image, ScrollView, XStack } from "tamagui";
 
 export default function LicenceAgreement() {
   const { t } = useTranslation("licence-agreement");
@@ -16,18 +16,24 @@ export default function LicenceAgreement() {
   const list = [t("l1"), t("l2")];
 
   return (
-    <Screen headerProps={{ title: t("header-title") }}>
+    <Screen
+      headerProps={{ title: t("header-title") }}
+      contentContainerStyle={{ backgroundColor: "white" }}
+    >
       <ScrollView
         contentContainerStyle={{ padding: "$lg", flexGrow: 1, gap: "$lg" }}
         bounces={false}
         showsVerticalScrollIndicator={false}
       >
+        <Image
+          source={require("../../assets/images/commit-global-logo.png")}
+          style={{ width: "100%", height: 70 }}
+        />
+
         <Typography preset="heading" textAlign="center" paddingHorizontal="$lg">
           {t("title")}
         </Typography>
-
         <Typography>{t("p1")}</Typography>
-
         {list.map((item) => (
           <XStack gap="$sm" key={item}>
             <Circle size={5} backgroundColor="$blue11" marginTop="$sm" />
