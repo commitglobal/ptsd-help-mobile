f2e1cd3e521eab40c0d4ef0da033571781b614c6
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2024-11-18 16:02:23 +0200

    chore: set correct import

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,12 +1,11 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_complete_human_torch.sql';
+import m0000 from './0000_i_message_migration.sql';
 
-  export default {
-    journal,
-    migrations: {
-      m0000
-    }
-  }
-  
+export default {
+  journal,
+  migrations: {
+    m0000,
+  },
+};
