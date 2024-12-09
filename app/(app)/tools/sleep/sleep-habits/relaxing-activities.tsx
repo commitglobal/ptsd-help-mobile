dc9d747bde545acb1b2d45b673f399249d6b98bb
Andrew Radulescu <radulescuandrew@gmail.com>
2024-12-09 12:01:09 +0200

    fix: updates for sleep

--- app/(app)/tools/sleep/sleep-habits/relaxing-activities.tsx
+++ app/(app)/tools/sleep/sleep-habits/relaxing-activities.tsx
@@ -1,32 +1,47 @@
 import { Icon } from '@/components/Icon';
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Screen } from '@/components/Screen';
 import useTranslationKeys from '@/hooks/useTranslationKeys';
 import { useTranslation } from 'react-i18next';
 import { router } from 'expo-router';
 import sleepActivitiesRepository, { SleepActivityType } from '@/db/repositories/sleep-activities.repository';
-import StarList from '@/components/StarList';
+import StarList, { StarListItem } from '@/components/StarList';
 import { useSleepActivitiesByType } from '@/services/sleep-activities.service';
 import * as Notifications from 'expo-notifications';
+import { Separator } from 'tamagui';
+import { Typography } from '@/components/Typography';
+import { XStack, YStack } from 'tamagui';
+import { Switch } from '@/components/Switch';
+import { format } from 'date-fns';
+import { TimePicker } from '@/components/TimePicker';
 
 export default function RelaxingActivities() {
   const { t } = useTranslation('tools');
   const { toolsTranslationKeys } = useTranslationKeys();
   const { data: sleepActivity, refetch } = useSleepActivitiesByType(SleepActivityType.RELAXING);
-  const [favoriteItems, setFavoriteItems] = useState<string[]>([]);
+  const [favoriteItems, setFavoriteItems] = useState<StarListItem[]>([]);
   const [isReminderChecked, setIsReminderChecked] = useState(false);
   const [time, setTime] = useState<Date | null>(null);
 
-  const activities = useMemo(() => {
+  useEffect(() => {
     const elements = t(toolsTranslationKeys.SLEEP.subcategories.SLEEP_HABITS.relaxingActivities, {
       returnObjects: true,
     });
-    return Object.values(elements).map((element) => ({
-      id: element.id,
-      label: element.text,
-      favorite: false,
-    }));
-  }, []);
+    const itemsToReturn = Object.values(elements)
+      .map((element) => ({
+        id: element.id,
+        label: element.text,
+        favorite: sleepActivity?.favorites?.includes(element.id) || false,
+      }))
+      .sort((a, b) => (b.favorite ? 1 : 0) - (a.favorite ? 1 : 0));
+
+    setFavoriteItems(itemsToReturn);
+  }, [sleepActivity]);
+
+  useEffect(() => {
+    setIsReminderChecked(sleepActivity?.notificationId !== null);
+    setTime(sleepActivity?.reminderTime ? new Date(sleepActivity?.reminderTime) : null);
+  }, [sleepActivity]);
 
   const handleDatabaseUpdate = async (updatedItems: Set<string>) => {
     const reminderTimeDate = time ? new Date(time) : new Date();
@@ -83,12 +98,19 @@ export default function RelaxingActivities() {
     }
   };
 
-  const handleOnUpdate = (updatedItems: string[]) => {
-    setFavoriteItems(updatedItems);
+  const handleOnUpdate = (updatedItem: StarListItem) => {
+    setFavoriteItems((prevItems) => {
+      return prevItems.map((item) => {
+        if (item.id === updatedItem.id) {
+          item.favorite = updatedItem.favorite;
+        }
+        return item;
+      });
+    });
   };
 
   const handleOnSave = async () => {
-    await handleDatabaseUpdate(new Set(favoriteItems));
+    await handleDatabaseUpdate(new Set(favoriteItems.filter((item) => item.favorite).map((item) => item.id)));
     router.back();
   };
 
@@ -117,14 +139,44 @@ export default function RelaxingActivities() {
         }}
         contentContainerStyle={{ backgroundColor: 'transparent' }}>
         <StarList
-          type={SleepActivityType.RELAXING}
-          activities={activities}
-          description={t(toolsTranslationKeys.SLEEP.subcategories.SLEEP_HABITS.relaxingActivitiesDescription)}
-          hasReminder={true}
-          reminderText={t(toolsTranslationKeys.SLEEP.subcategories.SLEEP_HABITS.relaxingActivitiesReminderTitle)}
+          listHeaderComponent={
+            <>
+              <Typography paddingBottom={8} preset='default'>
+                {t(toolsTranslationKeys.SLEEP.subcategories.SLEEP_HABITS.relaxingActivitiesDescription)}
+              </Typography>
+              <YStack backgroundColor='white' padding='$md' gap='$xs' borderRadius='$sm' marginBottom='$md'>
+                <XStack alignItems='center' justifyContent='space-between' gap='$md'>
+                  <Typography flex={1}>
+                    {t(toolsTranslationKeys.SLEEP.subcategories.SLEEP_HABITS.relaxingActivitiesReminderTitle)}
+                  </Typography>
+                  <Switch isChecked={isReminderChecked} setIsChecked={handleOnReminderChange} />
+                </XStack>
+
+                {isReminderChecked && (
+                  <YStack
+                    gap='$xs'
+                    animation='quick'
+                    enterStyle={{ opacity: 0, y: -10 }}
+                    exitStyle={{ opacity: 0, y: -10 }}
+                    y={0}
+                    opacity={1}>
+                    <Separator />
+
+                    <XStack justifyContent='space-between'>
+                      <Typography>{'ZILNIC'}</Typography>
+                      <Typography>{format(time || new Date(), 'HH:mm')}</Typography>
+                    </XStack>
+
+                    <XStack justifyContent='center'>
+                      <TimePicker date={time || new Date()} onChange={handleOnTimeChange} />
+                    </XStack>
+                  </YStack>
+                )}
+              </YStack>
+            </>
+          }
+          items={favoriteItems}
           update={handleOnUpdate}
-          onReminderChange={handleOnReminderChange}
-          onTimeChange={handleOnTimeChange}
         />
       </Screen>
     </>
